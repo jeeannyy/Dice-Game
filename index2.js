13d20188b145fc50f1f35bbffc044538c4650697
J <jeeannkim@gmail.com>
2021-02-04 20:38:17 +0900

    finish

---
+++ index2.js
@@ -0,0 +1,34 @@
+
+//First player setting
+const randomNumber1 = Math.floor(Math.random() * 6) + 1; 
+// returns a random number 1 to 6
+
+const randomImage = "dice" + randomNumber1 + ".png";
+// string dice1.png - dice6.png
+
+const randomImageSrc = "images/" + randomImage;
+
+const player1 = document.querySelectorAll("img")[0].setAttribute("src", randomImageSrc);
+// 우선 엘리먼트를 선택해야함, 그 다음에 바꿔줌  + (img 2개 중 첫 번째 img니까)
+
+
+//Second player setting
+const randomNumber2 = Math.floor(Math.random() * 6) + 1; 
+
+const randomImage2 = "dice" + randomNumber2 + ".png";
+
+const randomImageSrc2 = "images/" + randomImage2;
+
+const player2 = document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
+
+
+// title change depend on winner
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = " 🥂 Player 1 wins!";
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector("h1").innerHTML = " Player 2 wins! 🥂";
+} else {
+    document.querySelector("h1").innerHTML = " Draw!";
+
+}
